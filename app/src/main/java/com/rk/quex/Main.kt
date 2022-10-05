70dcbd0b5d7558be5855ba6823f17a71e31b430c
Kirion <rznkolds@outlook.com>
2022-10-05 17:24:34 +0300

    First phase of the project

---
+++ app/src/main/java/com/rk/quex/Main.kt
@@ -0,0 +1,11 @@
+package com.rk.quex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
