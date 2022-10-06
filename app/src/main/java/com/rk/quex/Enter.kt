01adca73f96f64b0cd1c4a2a6dbed23a730741f2
Kirion <rznkolds@outlook.com>
2022-10-06 14:11:06 +0300

    Added fragment for user login ( Enter.kt )

--- app/src/main/java/com/rk/quex/Enter.kt
+++ app/src/main/java/com/rk/quex/Enter.kt
@@ -1,16 +1,15 @@
 package com.rk.quex
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 
-class Create : Fragment() {
+class Enter : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-
-        return inflater.inflate(R.layout.fragment_create, container, false)
+        return inflater.inflate(R.layout.fragment_enter, container, false)
     }
 }
