643ebded7b08dd95be2e3ab85cae52a7e1b59913
Kirion <rznkolds@outlook.com>
2022-10-07 21:07:28 +0300

    Firebase connected and view binding enabled

--- app/src/main/java/com/rk/quex/pieces/Create.kt
+++ app/src/main/java/com/rk/quex/pieces/Create.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.rk.quex.R
+import com.rk.quex.databinding.FragmentCreateBinding
+import com.rk.quex.databinding.FragmentEnterBinding
 
 class Create : Fragment() {
 
+    private lateinit var binding : FragmentCreateBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+        binding = FragmentCreateBinding.inflate(inflater , container ,false )
 
-        return inflater.inflate(R.layout.fragment_create, container, false)
+        binding.enterScreen.setOnClickListener {
+
+            this.findNavController().navigate(R.id.action_create_to_enter)
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/com/rk/quex/pieces/Enter.kt
+++ app/src/main/java/com/rk/quex/pieces/Enter.kt
@@ -8,12 +8,21 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.rk.quex.R
+import com.rk.quex.databinding.FragmentEnterBinding
 
 class Enter : Fragment() {
 
+    private lateinit var binding : FragmentEnterBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+        binding = FragmentEnterBinding.inflate(inflater , container ,false )
 
-        return inflater.inflate(R.layout.fragment_enter, container, false)
+        binding.createScreen.setOnClickListener {
+
+            this.findNavController().navigate(R.id.action_enter_to_create)
+        }
+
+        return binding.root
     }
 }
