8a5f862dcf7bcf08a4ed846818d14621f1a6394b
Kirion <rznkolds@outlook.com>
2022-10-07 19:04:28 +0300

    Edited xml file of Create screen

--- app/src/main/java/com/rk/quex/pieces/Enter.kt
+++ app/src/main/java/com/rk/quex/pieces/Enter.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.rk.quex.R
 
 class Enter : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
 
         return inflater.inflate(R.layout.fragment_enter, container, false)
     }
